eef4a013efe1a1325bb600c30c6375fb18df956a
miragee37 <240404049@ogr.kent.edu.tr>
2026-04-02 15:24:04 +0300

    Added isTrigger code and made the "pickup" boxes pickable.

--- RollABallGame/Assets/Scripts/PlayerController.cs
+++ RollABallGame/Assets/Scripts/PlayerController.cs
@@ -27,4 +27,12 @@ public class PlayerController : MonoBehaviour
 
         rb.AddForce(movement * speed);
     }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pickup"))
+        {
+            other.gameObject.SetActive(false);
+        }
+        
+    }
 }
